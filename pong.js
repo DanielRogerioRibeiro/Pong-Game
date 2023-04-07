37504220b59b5693755bbc2b2885f4554a971918
Daniel Rogerio Ribeiro <danielrogerioribeiro.dev@gmail.com>
2023-04-07 18:16:31 -0300

    Implementing Racket Collision Check function

--- pong.js
+++ pong.js
@@ -68,9 +68,11 @@ function movimentaMinhaRaquete() {
       yRaquete += 10;
   }
 }
-  // Função para verificar a colisão com a minha Raquete;
-  function verificaColisaoRaquete() {
-    if (xBolinha - raio < xRaquete + raqueteComprimento) {
-        velocidadeXBolinha *= -1;
+// Função para verificar a colisão com a minha Raquete;
+  function verificaColisaoRaquete(){
+    if (xBolinha - raio < xRaquete + raqueteComprimento && 
+        yBolinha - raio < yRaquete + raqueteAltura && 
+        yBolinha + raio > yRaquete){
+      velocidadeXBolinha *= -1;
     }
 }
