5a246de71d6976fdcd28156bc7b894bf07dabff7
Daniel Rogerio Ribeiro <danielrogerioribeiro.dev@gmail.com>
2023-04-08 21:14:37 -0300

    Including function for Opponent Racket Check, excluding show opponent racket function.

--- pong.js
+++ pong.js
@@ -18,15 +18,15 @@ function setup() {
 function draw() {
   background(0);
   mostraBolinha();
-  //movimentaBolinha();
+  movimentaBolinha();
   verificaColisaoBorda();
   mostraRaquete(xRaquete, yRaquete);
   movimentaMinhaRaquete();
   //verificaColisaoRaquete();
-  colisaoMinhaRaqueteBiblioteca();
+  verificaColisaoRaquete(xRaquete, yRaquete);
   mostraRaquete(xRaqueteOponente, yRaqueteOponente);
-  mostraRaqueteOponente
   movimentaRaqueteOponente();
+  verificaColisaoRaquete(xRaqueteOponente, yRaqueteOponente);
 }
 
 
@@ -58,6 +58,7 @@ let xRaquete = 5;
 let yRaquete = 150;
 let raqueteComprimento = 10;
 let raqueteAltura = 90;
+let colidiu = false;
 
 function mostraRaquete(x,y) {
   rect(x, y, raqueteComprimento, raqueteAltura);
@@ -72,30 +73,40 @@ function movimentaMinhaRaquete() {
       yRaquete += 10;
   }
 }
-// Função para verificar a colisão com a minha Raquete;
+
 function verificaColisaoRaquete(){
   if (xBolinha - raio < xRaquete + raqueteComprimento && 
       yBolinha - raio < yRaquete + raqueteAltura && 
       yBolinha + raio > yRaquete){
-      velocidadeXBolinha *= -1;
-    }
-
+    velocidadeXBolinha *= -1;
+  }
 }
+
+// Função para verificar a colisão com a minha Raquete;
+function verificaColisaoRaquete(x, y) {
+    colidiu = collideRectCircle(x, y, raqueteComprimento, raqueteAltura, xBolinha, yBolinha, raio);
+    if (colidiu){
+        velocidadeXBolinha *= -1;
+    }
+}
+
+
 //criando raquete do oponente
 //variáveis do oponente
 let xRaqueteOponente = 585;
 let yRaqueteOponente = 150;
 let velocidadeYOponente;
 
-// Função para mostrar raquete do oponente;
-function mostraRaqueteOponente(){
-  rect(xRaqueteOponente, yRaqueteOponente, raqueteComprimento, raqueteAltura);
-}
-
 function movimentaRaqueteOponente(){
   velocidadeYOponente = yBolinha - yRaqueteOponente - raqueteComprimento / 2 - 30;
   yRaqueteOponente += velocidadeYOponente
 }
 
+function colisaoRaqueteOponenteBiblioteca() {
+    colidiu = collideRectCircle(xRaqueteOponente, yRaqueteOponente, raqueteComprimento, raqueteAltura, xBolinha, yBolinha, raio);
+    if (colidiu){
+        velocidadeXBolinha *= -1;
+    }
+}
 
 
